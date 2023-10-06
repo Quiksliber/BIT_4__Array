7ea6d7fdfc580ecd9edff87f6e1ab9c47b92d8f5
Liang Xinyu <wlwlwl568423@outlook.com>
2023-10-06 14:04:55 +0800

    Update Project Bit - 4 - 2 (Three pieces) - game.h

--- Project Bit - 4 - 2 (Three pieces) - game.h
+++ Project Bit - 4 - 2 (Three pieces) - game.h
@@ -1,19 +1,19 @@
 #pragma once
 
-//头文件的包含
+//Inclusion of header files
 #include <stdio.h>
 #include <Windows.h>
 #include <stdlib.h>
 #include <time.h>
 
-//符号的定义
+//Definition of symbols
 #define ROW 3
 #define COL 3
 
-//游戏所含的函数的声明
-void menu();//目录菜单
-void main_game();//主游戏
-void board_initialization(char board[ROW][COL], int row, int col);//板面初始化
-void board_printing(char board[ROW][COL], int row, int col);//板面打印
-char win(char board[ROW][COL], int row, int col);//胜负判断
+//Declaration of functions contained in the game
+void menu();//Directory menu
+void main_game();//main game
+void board_initialization(char board[ROW][COL], int row, int col);//Board initialization
+void board_printing(char board[ROW][COL], int row, int col);//Board printing
+char win(char board[ROW][COL], int row, int col);//Judgement of victory or defeat
 void winner_printing(char outcome);
